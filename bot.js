3e915d9f561d83c6434c780ac2c74d96162b8e27
Willian <willianleonardorv@gmail.com>
2023-08-21 14:38:51 -0300

    ajustes

--- bot.js
+++ bot.js
@@ -23,22 +23,22 @@ function delay(t, v) {
 }
 
 //##Configuração Local
-const createConnection = async () => {
-	return await mysql.createConnection({
-		host: 'localhost',
-		user: 'root',
-		password: '',
-		database: 'whats'   
-	});
-}
+// const createConnection = async () => {
+// 	return await mysql.createConnection({
+// 		host: 'localhost',
+// 		user: 'root',
+// 		password: '',
+// 		database: 'teste'   
+// 	});
+// }
 
 //## Configuração VPS
 // const createConnection = async () => {
 // 	return await mysql.createConnection({
 // 		host: 'localhost',
-// 		user: 'portalalavan5JwT',
-// 		password: 'qRX2JaQpFVy57G8b1W9zfHAL',
-// 		database: 'portal_alavancaweb_com_br_anJUWDKO'   
+// 		user: 'alavanca_wsystem',
+// 		password: 'y53X6Y8pk^wX',
+// 		database: 'alavanca_WSystem'   
 // 	});
 // }
 
@@ -107,109 +107,109 @@ const savedSessions = carregarArquivoSessao();
 const sessionIndex = savedSessions.findIndex(sess => sess.id);
 const tok = savedSessions.splice(sessionIndex, 1)[0];
 
-const getUser = async (msgfom) => {
-	const connection = await createConnection();
-	const [rows] = await connection.execute('SELECT * FROM clientes_whats WHERE celular = ?', [msgfom]);
-  delay(1000).then(async function() {
-		 connection.end();
-		delay(500).then(async function() {
-			 connection.destroy();
-		});
-	});
-	if (rows.length > 0) {
-      return rows[0].data_interacao;
-  }
-	return "false";
-}
+// const getUser = async (msgfom) => {
+// 	const connection = await createConnection();
+// 	const [rows] = await connection.execute('SELECT * FROM clientes_whats WHERE celular = ?', [msgfom]);
+//   delay(1000).then(async function() {
+// 		 connection.end();
+// 		delay(500).then(async function() {
+// 			 connection.destroy();
+// 		});
+// 	});
+// 	if (rows.length > 0) {
+//       return rows[0].data_interacao;
+//   }
+// 	return "false";
+// }
 
-const getPeriod = async (cod_estabel) => {
-	const connection = await createConnection();
-	const [rows] = await connection.execute('SELECT * FROM saudacoes WHERE cod_estabel = ?', [cod_estabel]);
-  delay(1000).then(async function() {
-		 connection.end();
-		delay(500).then(async function() {
-			 connection.destroy();
-		});
-	});
-	if (rows.length > 0) {
-    const saudacaoPeriodicidade = [rows[0].periodicidade, rows[0].saudacao];
-      return saudacaoPeriodicidade;
-  }
-	return "false";
-}
+// const getPeriod = async (cod_estabel) => {
+// 	const connection = await createConnection();
+// 	const [rows] = await connection.execute('SELECT * FROM saudacoes WHERE cod_estabel = ?', [cod_estabel]);
+//   delay(1000).then(async function() {
+// 		 connection.end();
+// 		delay(500).then(async function() {
+// 			 connection.destroy();
+// 		});
+// 	});
+// 	if (rows.length > 0) {
+//     const saudacaoPeriodicidade = [rows[0].periodicidade, rows[0].saudacao];
+//       return saudacaoPeriodicidade;
+//   }
+// 	return "false";
+// }
 
-const getMens = async (cod_estabel) => {
-	const connection = await createConnection();
-	const [rows] = await connection.execute('SELECT * FROM mensagens WHERE cod_estabel = ?', [cod_estabel]);
-  delay(1000).then(async function() {
-		 connection.end();
-		delay(500).then(async function() {
-			 connection.destroy();
-		});
-	});
-	if (rows.length > 0) {
-      return rows;
-  }
-	return "false";
-}
+// const getMens = async (cod_estabel) => {
+// 	const connection = await createConnection();
+// 	const [rows] = await connection.execute('SELECT * FROM mensagens WHERE cod_estabel = ?', [cod_estabel]);
+//   delay(1000).then(async function() {
+// 		 connection.end();
+// 		delay(500).then(async function() {
+// 			 connection.destroy();
+// 		});
+// 	});
+// 	if (rows.length > 0) {
+//       return rows;
+//   }
+// 	return "false";
+// }
 
-const getHorarioFunc = async (cod_estabel, diaSemana) => {
-	const connection = await createConnection();
-	const [rows] = await connection.execute('SELECT * FROM ' + diaSemana + ' WHERE cod_estabel = ?', [cod_estabel]);
-  delay(1000).then(async function() {
-		 connection.end();
-		delay(500).then(async function() {
-			 connection.destroy();
-		});
-	});
-	if (rows.length > 0) {
-      return rows;
-  }
-	return "false";
-}
+// const getHorarioFunc = async (cod_estabel, diaSemana) => {
+// 	const connection = await createConnection();
+// 	const [rows] = await connection.execute('SELECT * FROM ' + diaSemana + ' WHERE cod_estabel = ?', [cod_estabel]);
+//   delay(1000).then(async function() {
+// 		 connection.end();
+// 		delay(500).then(async function() {
+// 			 connection.destroy();
+// 		});
+// 	});
+// 	if (rows.length > 0) {
+//       return rows;
+//   }
+// 	return "false";
+// }
 
-const getAusencia = async (cod_estabel) => {
-	const connection = await createConnection();
-	const [rows] = await connection.execute('SELECT * FROM ausencias WHERE cod_estabel = ?', [cod_estabel]);
-  delay(1000).then(async function() {
-		 connection.end();
-		delay(500).then(async function() {
-			 connection.destroy();
-		});
-	});
-	if (rows.length > 0) {
-      return rows[0].ausencia;
-  }
-	return "false";
-}
+// const getAusencia = async (cod_estabel) => {
+// 	const connection = await createConnection();
+// 	const [rows] = await connection.execute('SELECT * FROM ausencias WHERE cod_estabel = ?', [cod_estabel]);
+//   delay(1000).then(async function() {
+// 		 connection.end();
+// 		delay(500).then(async function() {
+// 			 connection.destroy();
+// 		});
+// 	});
+// 	if (rows.length > 0) {
+//       return rows[0].ausencia;
+//   }
+// 	return "false";
+// }
 
-const setUser = async (msgfom, nome, data, cod_estabel) => {
-	const connection = await createConnection();
-  const [rows] = await connection.execute('INSERT INTO `clientes_whats` (`id`,`nome`, `celular`, `data_interacao`, `cod_estabel`) VALUES (NULL,?,?,?,?)', [nome,msgfom,data,cod_estabel]);                             
-  delay(1000).then(async function() {
-    connection.end();
-   delay(500).then(async function() {
-      connection.destroy();
-   });
- });
- if (rows.length > 0) return true;
+// const setUser = async (msgfom, nome, data, cod_estabel) => {
+// 	const connection = await createConnection();
+//   const [rows] = await connection.execute('INSERT INTO `clientes_whats` (`id`,`nome`, `celular`, `data_interacao`, `cod_estabel`) VALUES (NULL,?,?,?,?)', [nome,msgfom,data,cod_estabel]);                             
+//   delay(1000).then(async function() {
+//     connection.end();
+//    delay(500).then(async function() {
+//       connection.destroy();
+//    });
+//  });
+//  if (rows.length > 0) return true;
 
- return false;
-}
+//  return false;
+// }
 
-const updateDateUser = async (msgfom, data) => {
-	const connection = await createConnection();
-  const [rows] = await connection.execute('UPDATE clientes_whats SET data_interacao = ? WHERE clientes_whats.celular = ?;', [data, msgfom]);                             
-  delay(1000).then(async function() {
-    connection.end();
-   delay(500).then(async function() {
-      connection.destroy();
-   });
- });
- if (rows.length > 0) return "true";
+// const updateDateUser = async (msgfom, data) => {
+// 	const connection = await createConnection();
+//   const [rows] = await connection.execute('UPDATE clientes_whats SET data_interacao = ? WHERE clientes_whats.celular = ?;', [data, msgfom]);                             
+//   delay(1000).then(async function() {
+//     connection.end();
+//    delay(500).then(async function() {
+//       connection.destroy();
+//    });
+//  });
+//  if (rows.length > 0) return "true";
 
- return "false";
-}
+//  return "false";
+// }
 
 const criarSessao = function(id, token,ativo) {
   const client = new Client({
@@ -278,146 +278,147 @@ const criarSessao = function(id, token,ativo) {
   });
 
   client.on('message', async(msg) => {
+    client.sendMessage(msg.from, "olá seja bem vindo");
     
-    async function salvaContato(dados){
-      try{
+    // async function salvaContato(dados){
+    //   try{
 
-        nomeContato = msg._data.notifyName;        
-        const user = msg.from.replace(/\D/g, '');  
-        const getUserFrom = await getUser(user);    
-        const mensagemSaudacao = await getPeriod(cod_estabel);        
+    //     nomeContato = msg._data.notifyName;        
+    //     const user = msg.from.replace(/\D/g, '');  
+    //     const getUserFrom = await getUser(user);    
+    //     const mensagemSaudacao = await getPeriod(cod_estabel);        
     
-        const data_interacao = function dataAtualFormatada(){
-          var data = new Date(),
-              dia  = data.getDate().toString(),
-              diaF = (dia.length == 1) ? '0'+dia : dia,
-              mes  = (data.getMonth()+1).toString(), //+1 pois no getMonth Janeiro começa com zero.
-              mesF = (mes.length == 1) ? '0'+mes : mes,
-              anoF = data.getFullYear();
-          return anoF+"/"+mesF+"/"+diaF;
-        } 
+    //     const data_interacao = function dataAtualFormatada(){
+    //       var data = new Date(),
+    //           dia  = data.getDate().toString(),
+    //           diaF = (dia.length == 1) ? '0'+dia : dia,
+    //           mes  = (data.getMonth()+1).toString(), //+1 pois no getMonth Janeiro começa com zero.
+    //           mesF = (mes.length == 1) ? '0'+mes : mes,
+    //           anoF = data.getFullYear();
+    //       return anoF+"/"+mesF+"/"+diaF;
+    //     } 
 
-        if (getUserFrom === "false") { 
-          const data = new Date();  
-          await setUser(user, nomeContato, data, cod_estabel);
+    //     if (getUserFrom === "false") { 
+    //       const data = new Date();  
+    //       await setUser(user, nomeContato, data, cod_estabel);
           
-          if(dados == 1){
-            if (msg.body !== ""){
-              client.sendMessage(msg.from, mensagemSaudacao[1]);
-            }
-          }      
-        }else{   
+    //       if(dados == 1){
+    //         if (msg.body !== ""){
+    //           client.sendMessage(msg.from, mensagemSaudacao[1]);
+    //         }
+    //       }      
+    //     }else{   
               
-          var day1 = new Date(getUserFrom);         
-          var day2 = new Date(data_interacao());
+    //       var day1 = new Date(getUserFrom);         
+    //       var day2 = new Date(data_interacao());
 
-          var difference= Math.abs(day2-day1);
-          var days = difference/(1000 * 3600 * 24)
+    //       var difference= Math.abs(day2-day1);
+    //       var days = difference/(1000 * 3600 * 24)
 
-          if(Math.trunc(days) >= mensagemSaudacao[0]){
+    //       if(Math.trunc(days) >= mensagemSaudacao[0]){
 
-            if(dados == 1){ 
-              client.sendMessage(msg.from, mensagemSaudacao[1]);
-            }
+    //         if(dados == 1){ 
+    //           client.sendMessage(msg.from, mensagemSaudacao[1]);
+    //         }
 
-            await updateDateUser(user, new Date());
-          }else{          
-              if(Math.trunc(days) !== 0){
-                await updateDateUser(user, new Date());
-              }          
-          }        
-        } 
-      }
-      catch(e){
-        console.log('Não foi possível armazenar o usuário' + e)
-      }    
-    }
+    //         await updateDateUser(user, new Date());
+    //       }else{          
+    //           if(Math.trunc(days) !== 0){
+    //             await updateDateUser(user, new Date());
+    //           }          
+    //       }        
+    //     } 
+    //   }
+    //   catch(e){
+    //     console.log('Não foi possível armazenar o usuário' + e)
+    //   }    
+    
 
-    async function buscaMensagens(){
-      const mensagens = await getMens(cod_estabel);
-      var mensUnica = "";
+    // async function buscaMensagens(){
+    //   const mensagens = await getMens(cod_estabel);
+    //   var mensUnica = "";
 
-      if(mensagens !== "false"){
-        mensagens.forEach(function (mensagens){
-          if(mensagens.status !== 'Inativo'){
-            if(msg.body.toLocaleLowerCase().includes(mensagens.pergunta.toLocaleLowerCase())){
+    //   if(mensagens !== "false"){
+    //     mensagens.forEach(function (mensagens){
+    //       if(mensagens.status !== 'Inativo'){
+    //         if(msg.body.toLocaleLowerCase().includes(mensagens.pergunta.toLocaleLowerCase())){
 
-              mensUnica = mensagens.resposta;
+    //           mensUnica = mensagens.resposta;
               
-            }
-          }
-        });
+    //         }
+    //       }
+    //     });
 
-        if(mensUnica !== ""){
-          client.sendMessage(msg.from, mensUnica);
-        }       
-      }
-    }
+    //     if(mensUnica !== ""){
+    //       client.sendMessage(msg.from, mensUnica);
+    //     }       
+    //   }
+    // }
 
-    async function horarioFunc(){
-      var dataAtual = new Date();
-      var diaDaSemana = dataAtual.getDay();
-      var nomeDia = "";
+    // async function horarioFunc(){
+    //   var dataAtual = new Date();
+    //   var diaDaSemana = dataAtual.getDay();
+    //   var nomeDia = "";
 
-      switch (diaDaSemana) {
-        case 0:
-          nomeDia = "domingos";
-          break;
-        case 1:
-          nomeDia = "segundas"
-          break;
-        case 2:
-          nomeDia = "tercas";
-          break;
-        case 3:
-          nomeDia = "quartas";
-          break;
-        case 4:
-          nomeDia = "quintas";
-          break;
-        case 5:
-          nomeDia = "sextas";
-          break;
-        case 6:
-          nomeDia = "sabados";
-          break;
-      }
+    //   switch (diaDaSemana) {
+    //     case 0:
+    //       nomeDia = "domingos";
+    //       break;
+    //     case 1:
+    //       nomeDia = "segundas"
+    //       break;
+    //     case 2:
+    //       nomeDia = "tercas";
+    //       break;
+    //     case 3:
+    //       nomeDia = "quartas";
+    //       break;
+    //     case 4:
+    //       nomeDia = "quintas";
+    //       break;
+    //     case 5:
+    //       nomeDia = "sextas";
+    //       break;
+    //     case 6:
+    //       nomeDia = "sabados";
+    //       break;
+    //   }
 
-      const hrsFunc = await getHorarioFunc(cod_estabel, nomeDia);
+    //   const hrsFunc = await getHorarioFunc(cod_estabel, nomeDia);
 
-      const date = new Date();
-      const horaAtual = date.getHours();
-      var  atual = "";
+    //   const date = new Date();
+    //   const horaAtual = date.getHours();
+    //   var  atual = "";
 
-      if(horaAtual < 10){
-             atual = "0" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-      }else{
-             atual = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-      }
-      const inicioAtendimento = hrsFunc[0].inicio;
-      const paradaAtendimento = hrsFunc[0].pausa;
-      const retornoAtendimento = hrsFunc[0].retorno;
-      const fimAtendimento = hrsFunc[0].fim;
-      const mensAusencia = await getAusencia(cod_estabel); 
+    //   if(horaAtual < 10){
+    //          atual = "0" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    //   }else{
+    //          atual = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    //   }
+    //   const inicioAtendimento = hrsFunc[0].inicio;
+    //   const paradaAtendimento = hrsFunc[0].pausa;
+    //   const retornoAtendimento = hrsFunc[0].retorno;
+    //   const fimAtendimento = hrsFunc[0].fim;
+    //   const mensAusencia = await getAusencia(cod_estabel); 
       
-      if(mensAusencia == 'false'){
-        mensAusencia = '';
-      }
+    //   if(mensAusencia == 'false'){
+    //     mensAusencia = '';
+    //   }
 
-      if (atual.substring(0,5) >= inicioAtendimento && atual.substring(0,5) < paradaAtendimento || (atual.substring(0,5) >= retornoAtendimento && atual.substring(0,5) < fimAtendimento)){
-          salvaContato(1); //1- retorna mensagem saudação 2- retorna mensagem estabelecimento fechado 
-          buscaMensagens();
-      }else{
-        salvaContato(2); 
-        client.sendMessage(msg.from, mensAusencia);
-      }     
-    }
+    //   if (atual.substring(0,5) >= inicioAtendimento && atual.substring(0,5) < paradaAtendimento || (atual.substring(0,5) >= retornoAtendimento && atual.substring(0,5) < fimAtendimento)){
+    //       salvaContato(1); //1- retorna mensagem saudação 2- retorna mensagem estabelecimento fechado 
+    //       buscaMensagens();
+    //   }else{
+    //     salvaContato(2); 
+    //     client.sendMessage(msg.from, mensAusencia);
+    //   }     
+    // }
 
-    if (msg.body !== null && !msg.from.includes('@g.us') && msg.type.toLocaleLowerCase() !== "ciphertext" && msg.type.toLocaleLowerCase() !== "e2e_notification" && msg.type.toLocaleLowerCase() !== ""){
+    // if (msg.body !== null && !msg.from.includes('@g.us') && msg.type.toLocaleLowerCase() !== "ciphertext" && msg.type.toLocaleLowerCase() !== "e2e_notification" && msg.type.toLocaleLowerCase() !== ""){
       
-      horarioFunc();
+    //   horarioFunc();
       
-    }
+    // }
 });
 
   client.on('auth_failure', function() {
